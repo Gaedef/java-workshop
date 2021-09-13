45ef019497bb6b4af064524b73e79222734a119f
gaedef <luisgael.delacruz@gmail.com>
2021-09-13 17:59:18 -0500

    Adding first Java program

---
+++ hello-world/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    private void sayHello(){
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        new HelloWorld().sayHello();
+    }
+}
