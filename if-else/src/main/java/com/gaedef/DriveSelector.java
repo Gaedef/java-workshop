128f5a255acd936475b6bb46a012fbe9ad4c0a5d
gaedef <luisgael.delacruz@gmail.com>
2021-09-15 16:31:42 -0500

    Adding tricycle and bicycle options

--- if-else/src/main/java/com/gaedef/DriveSelector.java
+++ if-else/src/main/java/com/gaedef/DriveSelector.java
@@ -1,4 +1,18 @@
 package com.gaedef;
 
 public class DriveSelector {
+
+  private int age;
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getDrive() {
+    if (age < 6) {
+      return "TRICYCLE";
+    } else {
+      return "BICYCLE";
+    }
+  }
 }
--- if-else/src/test/java/com/gaedef/DriveSelectorTest.java
+++ if-else/src/test/java/com/gaedef/DriveSelectorTest.java
@@ -3,15 +3,23 @@ package com.gaedef;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DriveSelectorTest {
 
-    private DriveSelector driveSelector = new DriveSelector();
+  private DriveSelector driveSelector = new DriveSelector();
 
-    @Test
-    @DisplayName("select a drive vehicle based in age")
-    void shouldSelectDrive(){
-        assertNotNull(driveSelector);
-    }
+  @Test
+  @DisplayName("select a tricycle vehicle based in age")
+  void shouldSelectTricycle() {
+    driveSelector.setAge(4);
+    assertEquals("TRICYCLE", driveSelector.getDrive());
+  }
+
+  @Test
+  @DisplayName("select a bicycle vehicle based in age")
+  void shouldSelectBicycle() {
+    driveSelector.setAge(8);
+    assertEquals("BICYCLE", driveSelector.getDrive());
+  }
 }
