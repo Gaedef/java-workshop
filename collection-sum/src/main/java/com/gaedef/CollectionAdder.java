fc62d2a160f77b41ab37c97b2a37a1d30d8c958f
gaedef <luisgael.delacruz@gmail.com>
2021-09-21 19:40:42 -0500

    Adding collection sum soluton

--- collection-sum/src/main/java/com/gaedef/CollectionAdder.java
+++ collection-sum/src/main/java/com/gaedef/CollectionAdder.java
@@ -3,7 +3,11 @@ package com.gaedef;
 import java.util.List;
 
 public class CollectionAdder {
-    public int sum(List<Integer> numbers) {
-        return numbers.size();
+  public int sum(List<Integer> numbers) {
+    int sum = 0;
+    for (Integer i : numbers) {
+      sum += i;
     }
+    return sum;
+  }
 }
